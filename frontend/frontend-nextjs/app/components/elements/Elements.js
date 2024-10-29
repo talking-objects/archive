0f9a39609f29d517168bac9129e9819f73ac771e
Dain Park <zndgn555@gmail.com>
2024-10-29 11:08:41 +0100

    update: Legend element

--- frontend/frontend-nextjs/app/components/elements/Elements.js
+++ frontend/frontend-nextjs/app/components/elements/Elements.js
@@ -1,17 +1,88 @@
+import { COLORS } from "@/app/utils/constant/etc"
+
+
+const ItemBoxWrapper = ({children}) => {
+   return <div className="flex items-center gap-2">{children}</div>
+}
+
+
+const CategoryItemBox = ({color, text}) => {
+   return (
+         <ItemBoxWrapper>
+            <div style={{backgroundColor: `${color}`}} className="w-4 h-8 border border-black"></div>
+            <div>{text}</div>
+         </ItemBoxWrapper>
+   )
+}
+
+const EtcItemBox = ({children, text}) => {
+   return <ItemBoxWrapper>
+   {children}
+   <div>{text}</div>
+</ItemBoxWrapper>  
+}
+
+const LegendBoxWrapper = ({children, title}) => {
+   return <div className="flex flex-col w-full border-b border-black pb-4">
+      <div className="text-2xl font-semibold mb-8">{title}</div>
+      {children}
+   </div>
+}
+
 
 // 📄useing : video page, edit page
 export const LegendContainer = ({onToggleLegend}) => {
     return <div className="w-screen h-[100svh] bg-white bg-opacity-90 absolute top-0 left-0 z-[50] px-4 py-4">
     <div className="w-full max-w-screen-2xl mx-auto">
        <div className="flex w-full justify-between items-center">
-       <div>Legend</div>
-       <div onClick={() => onToggleLegend(false)}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-          </svg>
+         <div className="text-4xl font-bold italic">Legend</div>
+         <div onClick={() => onToggleLegend(false)} className="cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-12">
+               <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
+         </div>
        </div>
+       <div className="grid grid-cols-2 mt-8 gap-4 gap-x-8">
+         <LegendBoxWrapper title={"Categories"}>
+            <div className="flex flex-wrap gap-8">
+               <CategoryItemBox color={COLORS.c1} text={"Identity"} />
+               <CategoryItemBox color={COLORS.c2} text={"Knowledge"} />
+               <CategoryItemBox color={COLORS.c3} text={"Artistic Reflections"} />
+               <CategoryItemBox color={COLORS.c4} text={"Restitution"} />
+               <CategoryItemBox color={COLORS.c5} text={"Memory and The Imaginary"} />
+            </div>
+         </LegendBoxWrapper>
+         <LegendBoxWrapper title={"Data"}>
+               
+         </LegendBoxWrapper>
+         <LegendBoxWrapper title={"Tags"}>
+            <EtcItemBox text={"Explanation here Loren Epsom"}>
+               <div style={{backgroundColor: `${COLORS.c6}`}} className="w-2 h-8 border border-black"></div>
+            </EtcItemBox>
+         </LegendBoxWrapper>
+         <LegendBoxWrapper title={"Events"}>
+            <EtcItemBox text={"Explanation here Loren Epsom"}>
+               <div style={{backgroundColor: `${COLORS.c6}`, borderColor: `${COLORS.c5}`}} className="w-8 h-8 border-[5px]"></div>
+            </EtcItemBox>
+         </LegendBoxWrapper>
+         <LegendBoxWrapper title={"References"}>
+            <EtcItemBox text={"Explanation here Loren Epsom"}>
+               <div style={{backgroundColor: `${COLORS.c0}`, borderColor: `${COLORS.c4}`}} className="w-8 h-8 border-[5px] rounded-full"></div>
+            </EtcItemBox>
+         </LegendBoxWrapper>
+         <LegendBoxWrapper title={"Places"}>
+            <EtcItemBox text={"Explanation here Loren Epsom"}>
+               <div style={{backgroundColor: `${COLORS.c6}`, borderColor: `${COLORS.c4}`}} className="w-8 h-8 border-[5px] rounded-full"></div>
+            </EtcItemBox>
+         </LegendBoxWrapper>
+         <LegendBoxWrapper title={"Narrations"}>
+            <EtcItemBox text={"Explanation here Loren Epsom"}>
+               <div style={{backgroundColor: `${COLORS.c2}`}} className="w-8 h-8 border-black border rounded-full"></div>
+            </EtcItemBox>
+         </LegendBoxWrapper>
        </div>
     </div>
+    
 </div>
 }
 
--- frontend/frontend-nextjs/app/utils/constant/etc.js
+++ frontend/frontend-nextjs/app/utils/constant/etc.js
@@ -1,1 +1,12 @@
 export const BASE_URL = "https://talkingobjects.0x2620.org";
+
+export const COLORS = {
+    c0: "#FFFFFF",
+    c1: "#9E21E8",
+    c2: "#8BA5F8",
+    c3: "#691220",
+    c4: "#EC6735",
+    c5: "#F1A73D",
+    c6: "#3118E8"
+}
+
