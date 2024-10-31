4e4c2136112d1555100cf844461c00e9f4ed4369
Dain Park <zndgn555@gmail.com>
2024-10-31 17:51:53 +0100

    update: forest page infoLabel

--- frontend/frontend-nextjs/app/forest/components/ForestWrapper.js
+++ frontend/frontend-nextjs/app/forest/components/ForestWrapper.js
@@ -4,8 +4,34 @@ import { getAllAnnotations, getAllAnnotationsCounts, getAllClips, getAllVideos, 
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
+
+const InfoLable = ({children}) => {
+    return <div className="group-hover:flex hidden absolute top-0 left-[32px] bg-neutral-800 px-1 py-1 border-[0.5px] border-neutral-300 text-white rounded-md w-fit text-xs whitespace-nowrap">{children}</div>
+}
 const AnnotationIcon = ({layer}) => {
-    return <div className="w-8 h-8 bg-white flex justify-center items-center">
+    const getLayerName = (layer) => {
+        if(layer === "eventList"){
+            return "Event layer"
+        }
+        if(layer === "tagList"){
+            return "Tag layer"
+        }
+        if(layer === "placeList"){
+            return "Place layer"
+        }
+        if(layer === "refList"){
+            return "Reference layer"
+        }
+        if(layer === "narrationList"){
+            return "Narration layer"
+        }
+        if(layer === "categoryList"){
+            return "Category layer"
+        }
+        return "Error say to DainDev"
+    }
+    return <div className="w-8 h-8 bg-white flex justify-center items-center cursor-help group relative">
+         <InfoLable><span>{getLayerName(layer)}</span></InfoLable>
         {layer === "categoryList" && <div className="w-2/3 aspect-square grid grid-cols-2 text-white text-xs font-medium ">
             <div style={{backgroundColor: `${COLORS.c6}`}} className="w-full h-full"></div>
             <div style={{backgroundColor: `${COLORS.c5}`}} className="w-full h-full"></div>
@@ -21,12 +47,14 @@ const AnnotationIcon = ({layer}) => {
 }
 
 const RawIcon = () => {
-    return  <div className="w-8 h-8 bg-white flex justify-center items-center">
+    return  <div className="w-8 h-8 bg-white flex justify-center items-center cursor-help group relative">
+        <InfoLable className="group-hover:flex hidden absolute top-[0px] left-[32px] bg-white text-black w-fit max-w-20 text-xs"><span>Raw Video</span></InfoLable>
         <div className="w-2/3 aspect-square bg-black rounded-full flex justify-center items-center text-white text-xs font-medium">R</div>
         </div>
 }
 const ClipIcon = () => {
-    return  <div className="w-8 h-8 bg-white flex justify-center items-center">
+    return  <div className="w-8 h-8 bg-white flex justify-center items-center cursor-help group relative">
+        <InfoLable className="group-hover:flex hidden absolute top-0 left-[32px] bg-white text-black w-fit max-w-20 text-xs"><span>Clip</span></InfoLable>
         <div className="w-2/3 aspect-square bg-black rounded-full flex justify-center items-center text-white text-xs font-medium">C</div>
         </div>
 }
@@ -69,16 +97,16 @@ const ForestContentsContainer = ({isLoading=true, allData}) => {
     }
     
     return <div className="w-full py-4 h-fit">
-        {isLoading && <div className="w-full rounded-lg bg-[#8BA5F8] h-[450px] animate-pulse"></div>}
+        {isLoading && <div className="w-full rounded-lg bg-[#fff] h-[450px] animate-pulse"></div>}
         {(!isLoading) && <div className="w-full grid grid-cols-4 gap-4">
             {allData && allData.map((val, idx) => {
-                return <div key={idx} onClick={() => onPush(val)} className="flex w-full cursor-pointer">
+                return <div key={idx} className="flex w-full">
                             <div className="w-8 flex flex-col h-full">
                                 {val.type === "R" && <RawIcon />}
-                                {val.type === "C" && <ClipIcon />}
+                                {(val.type === "C" || val.type === "A" )&& <ClipIcon />}
                                 {val.type === "A" && <AnnotationIcon layer={val.layer} />}
                             </div>
-                        <div className="w-full h-full">
+                        <div onClick={() => onPush(val)} className="w-full h-full cursor-pointer">
                             <ForestContentsImageBox val={val} />
                             <div className="w-fit text-xs mt-2 font-semibold italic">{val.title}</div>
                         </div>
@@ -129,9 +157,10 @@ const ForestWrapper = () => {
     },[data, dataClips, dataAnnotations])
     
     return <>
-        <div className="w-full h-full flex flex-col items-center">
+        <div className="w-full h-full flex flex-col items-center relative">
             <div className="w-full h-fit py-4 flex justify-center items-center text-7xl font-medium">Our Archive</div>
             <div className="w-full h-[100svh] bg-black"></div>
+            <div className="w-full h-[62px] bg-[#8BA5F8] sticky top-0 left-0 z-[30]"></div>
                 <ContentContainer>
                     <ForestContentsContainer isLoading={(isLoading || isLoadingClips || isLoadingAnnotations)} allData={allData} />
               
