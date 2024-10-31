ace74cb1468e9d7625aa2ba3759ed50b34aac6e8
Dain Park <zndgn555@gmail.com>
2024-10-31 15:56:08 +0100

    update: forest marker

--- frontend/frontend-nextjs/app/forest/components/ForestWrapper.js
+++ frontend/frontend-nextjs/app/forest/components/ForestWrapper.js
@@ -57,20 +57,46 @@ const ClipIcon = () => {
         </div>
 }
 
+const ForestContentsImageBox = ({val}) => {
+    const [getId, setId] = useState(null);
+    const [getPostFrame, setPostFrame] = useState(null);
+    useEffect(() => {
+        if(val.type === "R"){
+            setId(val.id)
+            setPostFrame(val.posterFrame)
+        }
+        if(val.type === "C"){
+            setId(val.id.split("/")[0])
+            setPostFrame(val.in)
+        }
+    },[])
+    return <div style={{backgroundImage: `url(${BASE_URL}/${getId}/480p${getPostFrame}.jpg)`}} className="w-full aspect-video bg-neutral-200 rounded-md overflow-hidden bg-no-repeat bg-cover bg-center" />
+}
 
 const ForestContentsContainer = ({isLoading=true, allData}) => {
+    const router = useRouter();
+
+    const onPush = (value) => {
+        if(value.type === "R"){
+            router.push(`/video/${value.id}`)
+        }
+        if(value.type === "C"){
+            const id = value.id.split("/")[0]
+            router.push(`/clip/${id}?clipId=${value.id}&id=${id}`)
+        }
+    }
     
     return <div className="w-full py-4 h-fit">
         {isLoading && <div className="w-full rounded-lg bg-[#8BA5F8] h-[450px] animate-pulse"></div>}
         {(!isLoading) && <div className="w-full grid grid-cols-4 gap-4">
             {allData && allData.map((val, idx) => {
-                return <div key={idx} onClick={() => onPush(`/video/${val.id}`)} className="flex w-full cursor-pointer">
+                return <div key={idx} onClick={() => onPush(val)} className="flex w-full cursor-pointer">
                             <div className="w-8 flex flex-col h-full">
                                 {val.type === "R" && <RawIcon />}
                                 {val.type === "C" && <ClipIcon />}
                             </div>
                         <div className="w-full h-full">
-                            <div style={{backgroundImage: `url(${BASE_URL}/${val.id}/480p${val.posterFrame}.jpg)`}} className="w-full aspect-video bg-neutral-200 rounded-md overflow-hidden bg-no-repeat bg-cover bg-center" />
+                            <ForestContentsImageBox val={val} />
                             <div className="w-fit text-xs mt-2 font-semibold italic">{val.title}</div>
                         </div>
                    
@@ -91,7 +117,7 @@ const ForestWrapper = () => {
     // const {data:dataAnnotations, isLoading:isLoadingAnnotations} = getAllAnnotations({rangeToggle:true, range: [0, 12]})
     useEffect(() => {
         if(!isLoading && !isLoadingClips){
-            
+           
             data.data.items.map((v) => {
                 v.type = "R"
                 return v
@@ -106,12 +132,14 @@ const ForestWrapper = () => {
     return <>
         <div className="w-full h-full flex flex-col items-center">
             <div className="w-full h-fit py-4 flex justify-center items-center text-7xl font-medium">Our Archive</div>
-            <ContentContainer>
-                <ForestContentsContainer isLoading={(isLoading || isLoadingClips)} allData={allData} />
-                {/* <VideosContainer data={data} isLoading={isLoading} dataCount={dataCount} isLoadingCount={isLoadingCount} /> */}
-                {/* <AnnotationsContainer data={dataAnnotations} isLoading={isLoadingAnnotations} /> */}
-            </ContentContainer>
-            <div onClick={() => setPagination((prev) => prev + 1)} className="py-4 cursor-pointer">Load More</div>
+                <ContentContainer>
+                    <ForestContentsContainer isLoading={(isLoading || isLoadingClips)} allData={allData} />
+                    {/* <VideosContainer data={data} isLoading={isLoading} dataCount={dataCount} isLoadingCount={isLoadingCount} /> */}
+                    {/* <AnnotationsContainer data={dataAnnotations} isLoading={isLoadingAnnotations} /> */}
+                    {(!isLoading || !isLoadingClips) && <div className="w-full py-4 flex justify-center items-center">
+                        <div onClick={() => setPagination((prev) => prev + 1)} className="py-4 px-4 w-fit h-fit border border-black rounded-full cursor-pointer">Load More</div>
+                    </div>}
+                </ContentContainer>
         </div>
     </>
 }
