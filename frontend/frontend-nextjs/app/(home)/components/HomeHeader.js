25560c8853731aa4752fd7ba788a7a106582b342
Dain Park <zndgn555@gmail.com>
2024-12-06 10:52:54 +0100

    Create: HomePage and Video Pgae for 01.17 Launching EVA

---
+++ frontend/frontend-nextjs/app/(home)/components/HomeHeader.js
@@ -0,0 +1,11 @@
+const HomeHeader = () => {
+    return <div className="w-screen h-[100svh] pt-[56px] bg-black">
+        <div className="w-full h-full bg-red-400 relative">
+            <div className="w-full h-full bg-black absolute left-0 top-1/2 -translate-y-1/2">
+
+            </div>
+        </div>
+    </div>
+}
+
+export default HomeHeader;
