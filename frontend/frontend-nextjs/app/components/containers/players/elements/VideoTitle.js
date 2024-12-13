4a34b8283568fc2aa68a47e84ac163ab5ce0839b
Dain Park <zndgn555@gmail.com>
2024-12-13 17:11:23 +0100

    update

--- frontend/frontend-nextjs/app/components/containers/players/elements/VideoTitle.js
+++ frontend/frontend-nextjs/app/components/containers/players/elements/VideoTitle.js
@@ -1,5 +1,5 @@
 const VideoTitle = ({text}) => {
-    return  <span className="w-full flex lg:w-4/5 text-black text-2xl md:text-4xl lg:text-5xl font-ibm_mono_bolditalic italic transition-all duration-1000">
+    return  <span className="w-full flex overflow-hidden lg:w-4/5 text-black text-2xl md:text-4xl lg:text-5xl font-ibm_mono_bolditalic italic transition-all duration-1000">
     <div className="h-full bg-[#8BA5F8] px-2 lg:px-4"></div>
     <div>
       <span className=" inline bg-[#8BA5F8] leading-tight">{text}</span>
